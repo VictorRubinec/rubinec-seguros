e3a287e58bb0568ac4f73e2cce95a6086ae4ea0e
VictorRubinec <vrubinec@gmail.com>
2026-03-19 09:48:54 -0300

    adicionando config.js com chaves do EmailJs

---
+++ config.js
@@ -0,0 +1,11 @@
+// window.env = {
+//     EMAILJS_PUBLIC_KEY: 'your-public-key-here',
+//     EMAILJS_TEMPLATE_ID: 'your-template-id-here',
+//     EMAILJS_SERVICE_ID: 'your-service-id-here',
+// };
+
+window.env = {
+  EMAILJS_PUBLIC_KEY: "vb0edIpgnEmtHP5Bp",
+  EMAILJS_TEMPLATE_ID: "template_injnnjc",
+  EMAILJS_SERVICE_ID: "service_aapv7s6",
+};
